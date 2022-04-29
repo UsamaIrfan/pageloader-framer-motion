60900e4da4e11f34c47cd3f36eb29a2cc802aac7
Usama Irfan <usama.irfan9797@gmail.com>
2022-04-29 16:29:59 +0500

    Final

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,26 +1,92 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+const CUBIC_BEZIER = [0.6, 0.01, -0.05, 0.95];
+// Variants
+
+const banner = {
+  animate: {
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const letterAnimation = {
+  initial: {
+    y: 400,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      ease: CUBIC_BEZIER,
+      duration: 1,
+    },
+  },
+};
+
+const scrollDown = {
+  initial: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      ease: "easeInOut",
+      duration: 1,
+      delay: 1.8,
+    },
+  },
+};
+
+const bannerSubtitleText = {
+  initial: {
+    opacity: 0,
+    y: 80,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+
+    transition: {
+      ease: "easeInOut",
+      duration: 1,
+      delay: 0.4,
+    },
+  },
+};
 
 const Banner = () => {
   const [playMarquee, setPlayMarquee] = useState(false);
 
   useEffect(() => {
-    setPlayMarquee(true);
+    setTimeout(() => {
+      setPlayMarquee(true);
+    }, 2000);
   }, []);
   return (
-    <div className="banner">
+    <motion.div variants={banner} className="banner">
       <BannerRowTop title={"brand"} />
       <BannerRowCenter title={"experience"} playMarquee={playMarquee} />
       <BannerRowBottom title={"studio"} />
-    </div>
+    </motion.div>
   );
 };
 
-const AnimatedLetters = ({ title }) => (
-  <span className="row-title">
+const AnimatedLetters = ({ title, disabled }) => (
+  <motion.span
+    variants={disabled ? null : banner}
+    initial="initial"
+    animate="animate"
+    className="row-title"
+  >
     {[...title].map((letter) => (
-      <span className="row-letter">{letter}</span>
+      <motion.span variants={letterAnimation} className="row-letter">
+        {letter}
+      </motion.span>
     ))}
-  </span>
+  </motion.span>
 );
 
 const BannerRowTop = ({ title }) => {
@@ -29,12 +95,17 @@ const BannerRowTop = ({ title }) => {
       <div className="row-col">
         <AnimatedLetters title={title} />
       </div>
-      <div className="row-col">
+      <motion.div
+        variants={bannerSubtitleText}
+        initial="initial"
+        animate="show"
+        className="row-col"
+      >
         <span className="row-message">
           We are specialised in setting up the foundation of your brand and
           setting you up for success.
         </span>
-      </div>
+      </motion.div>
     </div>
   );
 };
@@ -42,10 +113,21 @@ const BannerRowTop = ({ title }) => {
 const BannerRowBottom = ({ title }) => {
   return (
     <div className={"banner-row center"}>
-      <div className="scroll">
-        <span>scroll</span>
-        <span>down</span>
-      </div>
+      <motion.div
+        initial={{
+          scale: 0,
+        }}
+        animate={{ scale: 1 }}
+        transition={{ ease: CUBIC_BEZIER, duration: 1, delay: 1 }}
+        className="scroll"
+      >
+        <motion.span variants={scrollDown} initial="initial" animate="show">
+          scroll
+        </motion.span>
+        <motion.span variants={scrollDown} initial="initial" animate="show">
+          down
+        </motion.span>
+      </motion.div>
       <AnimatedLetters title={title} />
     </div>
   );
@@ -55,10 +137,10 @@ const BannerRowCenter = ({ title, playMarquee }) => {
   return (
     <div className={`banner-row marquee  ${playMarquee && "animate"}`}>
       <div className="marquee__inner">
+        <AnimatedLetters title={title} disabled />
         <AnimatedLetters title={title} />
-        <AnimatedLetters title={title} />
-        <AnimatedLetters title={title} />
-        <AnimatedLetters title={title} />
+        <AnimatedLetters title={title} disabled />
+        <AnimatedLetters title={title} disabled />
       </div>
     </div>
   );
--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,41 +1,92 @@
 import React, { useEffect } from "react";
 import Image from "./Image";
+import { motion } from "framer-motion";
+
+const container = {
+  show: {
+    transition: {
+      staggerChildren: 0.35,
+    },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0,
+    y: 200,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: -200,
+    transition: {
+      ease: "easeInOut",
+      duration: 0.8,
+    },
+  },
+};
+
+const itemMain = {
+  hidden: {
+    opacity: 0,
+    y: 200,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 1.6,
+    },
+  },
+};
 
 const Loader = ({ setLoading }) => {
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 4000);
-    return () => clearTimeout(timer);
-  });
-
   return (
-    <div className='loader'>
-      <div className='loader-inner'>
-        <ImageBlock id='image-1' />
-        <div className='transition-image'>
-          <img
+    <div className="loader">
+      <motion.div
+        variants={container}
+        initial="hidden"
+        animate="show"
+        exit="exit"
+        className="loader-inner"
+        onAnimationComplete={() => setLoading(false)}
+      >
+        <ImageBlock variants={item} id="image-1" />
+        <motion.div variants={itemMain} className="transition-image">
+          <motion.img
+            transition={{
+              ease: [0.6, 0.01, -0.05, 0.95],
+              duration: 1.6,
+            }}
+            layoutId="main-image-1"
             src={process.env.PUBLIC_URL + `/images/image-2.jpg`}
-            alt='random alt'
+            alt="random alt"
           />
-        </div>
-        <ImageBlock id='image-3' />
-        <ImageBlock id='image-4' />
-        <ImageBlock id='image-5' />
-      </div>
+        </motion.div>
+        <ImageBlock variants={item} id="image-3" />
+        <ImageBlock variants={item} id="image-4" />
+        <ImageBlock variants={item} id="image-5" />
+      </motion.div>
     </div>
   );
 };
 
-export const ImageBlock = ({ id }) => {
+export const ImageBlock = ({ id, variants }) => {
   return (
-    <div className={`image-block ${id}`}>
+    <motion.div variants={variants} className={`image-block ${id}`}>
       <Image
         src={process.env.PUBLIC_URL + `/images/${id}.webp`}
         fallback={process.env.PUBLIC_URL + `/images/${id}.jpg`}
         alt={id}
       />
-    </div>
+    </motion.div>
   );
 };
 export default Loader;
